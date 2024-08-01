d450b30c4cf8f89f40c578d315d38c64b0d4b4e5
yuta-hamasaki <yh02052002@gmail.com>
2024-07-31 17:01:56 -0700

    finished

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 $(function() {
-  // your code here
+  $(".accordion-header").click(function() {
+    $(".accordion-content").slideUp();
+    if ($(this).next(".accordion-content").is(":hidden")) {
+      $(this).next(".accordion-content").slideDown();
+    }
+  });
+
+  $(".todos").find("button").click(function() {
+    $.ajax({
+      url: "https://dummyjson.com/todo",
+      type: "GET",
+      dataType: 'json',
+      success: function(res){
+        res.todos.forEach(function(data) {
+          $(".todos").find("ul").append(`<li>${data.todo}</li>`);
+        });
+        console.log("detail", res.todos)
+      },
+      error: function(error){
+        console.error(error)
+      }
+    })
+  })
+
 })
